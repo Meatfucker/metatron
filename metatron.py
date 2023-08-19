c0f14509e023367c5f840c7d223974f27e68eaa9
Meatfucker <tramshed@gmail.com>
2023-08-19 15:43:25 -0400

    Removed old unneeded dependencies.

--- metatron.py
+++ metatron.py
@@ -2,8 +2,6 @@ from typing import Optional
 import discord
 from discord import app_commands
 from discord import ui
-from discord.ext import commands
-from discord.ext.commands import Context
 import aiohttp
 import json
 import asyncio
